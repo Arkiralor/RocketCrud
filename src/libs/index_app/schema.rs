43e2165bb66fe3b2034f3542120358c236bc8960
Arkiralor <prithoo11335@gmail.com>
2023-06-09 05:44:20 +0530

    post test

--- src/libs/index_app/schema.rs
+++ src/libs/index_app/schema.rs
@@ -15,3 +15,37 @@ impl IndexResponse {
         Self { msg }
     }
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DummyRequest {
+    pub text: Option<String>,
+    pub msg: String,
+}
+
+impl DummyRequest {
+    pub fn create(t: Option<String>, m: Option<String>) -> Self {
+        let msg = match m {
+            Some(val) => val,
+            None => String::from(""),
+        };
+
+        Self { text: t, msg }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DummyResponse {
+    text: Option<String>,
+    msg: String,
+}
+
+impl DummyResponse {
+    pub fn create(t: Option<String>, m: Option<String>) -> Self {
+        let msg = match m {
+            Some(val) => val,
+            None => String::from(""),
+        };
+
+        Self { text: t, msg }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use rocket::{form::validate::contains, serde::json::*, *};
 mod libs;
 
 use crate::libs::index_app::helpers::index;
-use crate::libs::index_app::schema::IndexResponse;
+use crate::libs::index_app::schema::{DummyRequest, DummyResponse, IndexResponse};
 
 #[get("/")]
-fn index_controller() -> Json<IndexResponse> {
+fn get_index_controller() -> Json<IndexResponse> {
     let resp = IndexResponse::create(index());
+    Json(resp)
+}
+
+#[post("/", data = "<req>")]
+fn post_index_controller(req: Json<DummyRequest>) -> Json<DummyResponse> {
+    let resp = DummyResponse::create(req.text.clone(), Some(req.msg.clone()));
     Json(resp)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index_controller])
+    rocket::build().mount("/", routes![get_index_controller, post_index_controller])
 }
